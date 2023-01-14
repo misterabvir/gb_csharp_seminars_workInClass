e60167fb7dd320b1785a06b464578788fa3f7b49
misterabvir <abvir@yandex.ru>
2023-01-14 08:00:02 +0300

    add some comments

--- Seminar01/Examples003/Program.cs
+++ Seminar01/Examples003/Program.cs
@@ -1,18 +1,19 @@
-﻿/* Задача №3.Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-*  3 -> Среда 
-*  5 -> Пятница
-*/
+﻿/******************************************************************
+*  Задача №3.Напишите программу,                                  *
+*  которая будет выдавать название дня недели по заданному номеру.*
+*  3 -> Среда                                                     *
+*  5 -> Пятница                                                   *
+*******************************************************************/
+
 Console.Write("Input count day of week: ");
-int countOfWeek = Convert.ToInt32(Console.ReadLine());
+int dayOfWeekNumber = Convert.ToInt32(Console.ReadLine());
 
-
-
-if(countOfWeek == 1){Console.WriteLine("Monday");}
-else if (countOfWeek == 2) {Console.WriteLine("Tuesday");}
-else if (countOfWeek == 3) {Console.WriteLine("Wednesday");}
-else if (countOfWeek == 4) {Console.WriteLine("Thursday");}
-else if (countOfWeek == 5) {Console.WriteLine("Friday");}
-else if (countOfWeek == 6) {Console.WriteLine("Saturday");}
-else if (countOfWeek == 7) {Console.WriteLine("Sunday");}
-else {Console.WriteLine("Wrong input");}
+if(dayOfWeekNumber == 1) Console.WriteLine("Monday");
+else if(dayOfWeekNumber == 2) Console.WriteLine("Tuesday");
+else if(dayOfWeekNumber == 3) Console.WriteLine("Wednesday");
+else if(dayOfWeekNumber == 4) Console.WriteLine("Thursday");
+else if(dayOfWeekNumber == 5) Console.WriteLine("Friday");
+else if(dayOfWeekNumber == 6) Console.WriteLine("Saturday");
+else if(dayOfWeekNumber == 7) Console.WriteLine("Sunday");
+else Console.WriteLine("Wrong input");
 
