2bc83429b557874c8796a39c1eff5399fc094e01
misterabvir <abvir@yandex.ru>
2023-01-20 22:00:02 +0300

    added sln for tasks from seminar03

---
+++ Seminar03/Example_01/Program.cs
@@ -0,0 +1,14 @@
+﻿/****************************************************************************************
+* Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),   *
+* причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.*
+*****************************************************************************************/
+
+Console.Write("Input X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+     if(x > 0 && y > 0) Console.WriteLine("First quarter");
+else if(x < 0 && y > 0) Console.WriteLine("Second quarter");
+else if(x < 0 && y < 0) Console.WriteLine("Third quarter");
+else                    Console.WriteLine("Fourth quarter");
